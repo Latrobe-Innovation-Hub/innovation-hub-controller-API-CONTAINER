31c7de8e3d7f64aa8dde7ad448308884a783d981
famesjranko <andrewmcdonald42@gmail.com>
2023-07-23 03:05:24 +1000

    Update windows_host_config_tool.bat

--- host-setup/windows_host_config_tool.bat
+++ host-setup/windows_host_config_tool.bat
@@ -9,41 +9,61 @@ echo.
 echo Choose an option:
 echo 1. Install Windows Host Configurations and API Controller settings.
 echo 2. Uninstall Windows Host Configurations and revert to default.
-echo 3. Exit
+echo 3. Check Status of Changes
+echo 4. Exit
 
 set /p choice=Enter option number: 
 
 if "%choice%"=="1" (
-    echo Installing Windows Host Configurations and API Controller settings...
-    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-host.ps1" > "%~dp0install-log.txt" 2>&1
-    if %ERRORLEVEL% NEQ 0 (
-        echo An error occurred during installation. Please check the install-log.txt for details.
+    echo Checking status of changes...
+    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0status-checker.ps1"
+    if %ERRORLEVEL% EQU 0 (
+        echo Installing Windows Host Configurations and API Controller settings...
+        PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0install-host.ps1"
+        if %ERRORLEVEL% NEQ 0 (
+            echo An error occurred during installation. Please check the install-log.txt for details.
+        ) else (
+            echo Installation completed successfully.
+        )
     ) else (
-        echo Installation completed successfully.
+        echo Installation canceled by user.
     )
     pause
     goto MainMenu
 )
 
 if "%choice%"=="2" (
-    echo Are you sure you want to uninstall Windows Host Configurations and revert to default? (Y/N)
-    set /p uninstall_confirm=
-    if /i "%uninstall_confirm%"=="Y" (
-        echo Uninstalling Windows Host Configurations and reverting to default...
-        PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0uninstall-host.ps1" > "%~dp0uninstall-log.txt" 2>&1
-        if %ERRORLEVEL% NEQ 0 (
-            echo An error occurred during uninstallation. Please check the uninstall-log.txt for details.
+    echo Checking status of changes...
+    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0status-checker.ps1"
+    if %ERRORLEVEL% EQU 0 (
+        echo Are you sure you want to uninstall Windows Host Configurations and revert to default? (Y/N)
+        set /p uninstall_confirm=
+        if /i "%uninstall_confirm%"=="Y" (
+            echo Uninstalling Windows Host Configurations and reverting to default...
+            PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0uninstall-host.ps1"
+            if %ERRORLEVEL% NEQ 0 (
+                echo An error occurred during uninstallation. Please check the uninstall-log.txt for details.
+            ) else (
+                echo Uninstallation completed successfully.
+            )
         ) else (
-            echo Uninstallation completed successfully.
+            echo Uninstallation canceled.
         )
     ) else (
-        echo Uninstallation canceled.
+        echo Uninstallation canceled by user.
     )
     pause
     goto MainMenu
 )
 
 if "%choice%"=="3" (
+    echo Checking status of changes...
+    PowerShell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0host-checker.ps1"
+    pause
+    goto MainMenu
+)
+
+if "%choice%"=="4" (
     echo Exiting...
     exit /b
 )
