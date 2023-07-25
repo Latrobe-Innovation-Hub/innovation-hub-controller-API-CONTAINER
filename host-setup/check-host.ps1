b1257bd0c6324aba1a072f636bb53739ccd6ee79
famesjranko <andrewmcdonald42@gmail.com>
2023-07-25 19:05:06 +1000

    Update check-host.ps1

--- host-setup/check-host.ps1
+++ host-setup/check-host.ps1
@@ -19,15 +19,26 @@ $openSshInstalled = Test-Path "$env:ProgramFiles\OpenSSH\OpenSSH-Win64\ssh.exe"
 
 if ($openSshInstalled) {
     ShowStatus "Is OpenSSH installed?" $true
-    # Check if SSH services are running
-    $sshServiceStatus = Get-Service -Name sshd -ErrorAction SilentlyContinue
-    $sshAgentRunning = ($sshServiceStatus -ne $null) -and ($sshServiceStatus.Status -eq 'Running')
+
+    # Check if sshd service is running
+    $sshdStatus = Get-Service -Name sshd -ErrorAction SilentlyContinue
+    $sshdRunning = ($sshdStatus -ne $null) -and ($sshdStatus.Status -eq 'Running')
+    ShowStatus "Is sshd running?" $sshdRunning
+
+    # Check if sshd is set to auto start at boot
+    $sshdStartupType = (Get-Service -Name sshd -ErrorAction SilentlyContinue).StartType
+    $sshdAutoStart = ($sshdStartupType -eq 'Automatic')
+    ShowStatus "Is sshd set to auto start at boot?" $sshdAutoStart
+
+    # Check if ssh-agent service is running
+    $sshagentStatus = Get-Service -Name ssh-agent -ErrorAction SilentlyContinue
+    $sshAgentRunning = ($sshagentStatus -ne $null) -and ($sshagentStatus.Status -eq 'Running')
     ShowStatus "Is ssh-agent running?" $sshAgentRunning
 
-    # Check if ssh is set to auto start at boot
-    $sshStartupType = (Get-Service -Name sshd -ErrorAction SilentlyContinue).StartType
-    $sshAutoStart = ($sshStartupType -eq 'Automatic')
-    ShowStatus "Is ssh set to auto start at boot?" $sshAutoStart
+    # Check if ssh-agent is set to auto start at boot
+    $sshagentStartupType = (Get-Service -Name ssh-agent -ErrorAction SilentlyContinue).StartType
+    $sshagentAutoStart = ($sshagentStartupType -eq 'Automatic')
+    ShowStatus "Is ssh-agent set to auto start at boot?" $sshagentAutoStart
 } else {
     ShowStatus "Is OpenSSH installed?" $false
 }
