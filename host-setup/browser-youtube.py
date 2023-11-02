a2973c8628d654c7607f32c1f151d2304ca8cde9
administrator <administrator@debian-IH-controller.ltu.edu.au>
2023-11-02 16:47:51 +1100

    UPDATE: big, added more to control page specifically hosts section; and applied fix for youtube-host-script and api handling of it

--- host-setup/browser-youtube.py
+++ host-setup/browser-youtube.py
@@ -1,3 +1,4 @@
+from symbol import try_stmt
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
@@ -22,8 +23,8 @@ def toggle_captions(driver, enable_captions):
         captions_button.click()
 
 def play_youtube_video(youtube_url, loop_is_wanted, enable_captions):
-    pid = os.getpid()
-    print("PID:", pid)  # Print the PID to the console
+    #pid = os.getpid()
+    #print("PID:", pid)  # Print the PID to the console
 
     # Set up the Chrome driver with the --disable-infobars argument
     chrome_options = webdriver.ChromeOptions()
@@ -38,6 +39,47 @@ def play_youtube_video(youtube_url, loop_is_wanted, enable_captions):
 
     # Open the provided YouTube URL
     driver.get(youtube_url)
+
+    #from contextlib import suppress
+    import psutil
+
+    #found_pids = []
+    #for process in psutil.process_iter():
+    #  if process.name() == 'chrome.exe' and '--test-type=webdriver' in process.cmdline():
+    #          with suppress(psutil.NoSuchProcess):
+    #              print(process.pid)
+    #              found_pids.append(process.pid)
+
+    #if len(found_pids) >= 2:
+        #print("the second pid: ", found_pids[1])
+    #    print("the last pid: ", found_pids[-1])
+        
+
+    # Initialize variables to store main and child process PIDs
+    main_pid = None
+    child_pids = []
+
+    for process in psutil.process_iter():
+        try:
+            if process.name() == 'chrome.exe' and '--test-type=webdriver' in process.cmdline():
+                if main_pid is None or process.ppid() < main_pid:
+                    # Update the main_pid if this process has a lower PPID
+                    main_pid = process.pid
+                else:
+                    # Add other PIDs as child processes
+                    child_pids.append(process.pid)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+
+    #print("Main PID: ", main_pid)
+    print("PID:", main_pid)
+    #print("Child PIDs: ", child_pids)
+
+    if main_pid is not None:
+        # Write the main PID to the file
+        pid_file_path = f"youtube-pid.txt"
+        with open(pid_file_path, 'w') as pid_file:
+            pid_file.write(str(main_pid))
 
     print(f"Opened URL: {youtube_url}")
 
@@ -92,19 +134,24 @@ def play_youtube_video(youtube_url, loop_is_wanted, enable_captions):
 
     print("Closed context menu")
 
-    while True:
-        current_time = driver.execute_script("return document.querySelector('video').currentTime")
-        duration = driver.execute_script("return document.querySelector('video').duration")
+    try:
+        while True:
+            current_time = driver.execute_script("return document.querySelector('video').currentTime")
+            duration = driver.execute_script("return document.querySelector('video').duration")
 
-        if duration is not None and current_time >= duration:
-            break
+            if duration is not None and current_time >= duration:
+                break
 
-        time.sleep(1)
+            time.sleep(1)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        print("Video finished")
+        driver.quit()
 
-    print("Video finished")
-
-    # Close the browser after the video has finished
-    driver.quit()
+        pid_file_path = "youtube-pid.txt"
+        if os.path.exists(pid_file_path):
+            os.remove(pid_file_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Play a YouTube video with loop option and captions control")
@@ -117,5 +164,13 @@ if __name__ == "__main__":
     youtube_url = args.youtube_url
     loop_is_wanted = args.loop
     enable_captions = args.captions
-
-    play_youtube_video(youtube_url, loop_is_wanted, enable_captions)
+    
+    try:
+        play_youtube_video(youtube_url, loop_is_wanted, enable_captions)
+    except:
+        pass
+    finally:
+        # Remove the PID file
+        pid_file_path = f"youtube-pid.txt"
+        if os.path.exists(pid_file_path):
+            os.remove(pid_file_path)
